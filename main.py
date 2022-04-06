1f4db5d42d3044eb551c8853bf90cc028c2d1d87
xqwol <xqwol@yahoo.com>
2022-04-06 21:46:44 +0200

    CoMmEnTs

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import sys
 import pygame as pgm
-pgm.init()
+pgm.init() # initialise pygame components
 
 dimensions = w, h = 200,200
 
-gamewindow = pgm.display.set_mode(dimensions)
+gamewindow = pgm.display.set_mode(dimensions) #create window with specified dimensions
 
+
+# Run game:
 while 1:
-    for event in pgm.event.get():
-        if event.type == pgm.QUIT: sys.exit()
-    gamewindow.fill([0, 0, 0])
-    pgm.display.flip()
+    for event in pgm.event.get(): # listen for events
+        if event.type == pgm.QUIT: sys.exit() # close window if titlebar button pressed
+    gamewindow.fill([0, 0, 0]) # background color of the window
+    pgm.display.flip() # update the *entire* window
